2347db113825d312029df29b44145147c174067a
masih abedini <masih7816@gmail.com>
2023-11-29 02:56:57 +0330

    finish CheckOTPForm ui

---
+++ src/features/authentication/CheckOTPForm.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import OTPInput from "react-otp-input";
+
+const OTP_LENGTH = 6;
+
+const CheckOTPForm = () => {
+    const [otp, setOtp] = useState("");
+    const [isSubmittable, setIsSubmittable] = useState(false);
+
+    useEffect(() => {
+        if (otp.length === OTP_LENGTH) {
+            console.log("log In!");
+            setIsSubmittable(true);
+        } else {
+            setIsSubmittable(false);
+        }
+    }, [otp]);
+
+    return (
+        <form
+            className="flex flex-col p-5 rounded-lg bg-gray-50"
+            onSubmit={(e) => e.preventDefault()}
+        >
+            <h3 className="font-medium text-secondary-900 text-lg">
+                کد تایید را وارد کنید:
+            </h3>
+            <OTPInput
+                value={otp}
+                onChange={setOtp}
+                numInputs={OTP_LENGTH}
+                renderSeparator={<span>-</span>}
+                containerStyle="flex-row-reverse justify-center items-center gap-2 mt-3"
+                inputStyle="border border-primary-800 !w-12 !h-12 rounded-lg focus:border-primary-900 outline-none focus:shadow-md focus:shadow-primary-200 duration-300 disabled:bg-white disabled:opacity-30"
+                inputType="number"
+                shouldAutoFocus
+                renderInput={(props) => (
+                    <input onFocus={(e) => console.log(e.target)} {...props} />
+                )}
+            />
+            <button
+                type="submit"
+                className="app-btn disabled:opacity-70"
+                disabled={!isSubmittable}
+            >
+                ورود
+            </button>
+        </form>
+    );
+};
+
+export default CheckOTPForm;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,12 @@
+import CheckOTPForm from "../features/authentication/CheckOTPForm";
 import SendOTPForm from "../features/authentication/SendOTPForm";
 
 const Auth = () => {
     return (
         <div className="container">
             <div className="w-full min-h-screen flex items-center justify-center">
-                <SendOTPForm />
+                {/* <SendOTPForm /> */}
+                <CheckOTPForm />
             </div>
         </div>
     );
