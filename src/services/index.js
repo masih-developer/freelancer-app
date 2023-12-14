31f49185cc5eba2618c562658cf8969e0ad65707
masih abedini <masih7816@gmail.com>
2023-12-14 21:28:49 +0330

    add axios req and res interceptors

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,41 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: BASE_URL,
     withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+    (res) => res,
+    (err) => Promise.reject(err)
+);
+
+axiosInstance.interceptors.response.use(
+    (res) => res,
+    async (err) => {
+        const originalConfig = err.config;
+
+        if (err.response.status === 401 && !originalConfig._retry) {
+            originalConfig._retry = true;
+            try {
+                const { data } = await axios.get(
+                    `${BASE_URL}/user/refresh-token`,
+                    {
+                        withCredentials: true,
+                    }
+                );
+                if (data) {
+                    axiosInstance(originalConfig);
+                }
+            } catch (error) {
+                return Promise.reject(error);
+            }
+        }
+        return Promise.reject(err);
+    }
+);
 
 const mainRequest = {
     get: axiosInstance.get,
