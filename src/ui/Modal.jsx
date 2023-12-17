be0952e8e0191db5e2e24ffa9420a1059fc07304
masih abedini <masih7816@gmail.com>
2023-12-17 19:08:51 +0330

    add header title modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,7 @@
 import { createPortal } from "react-dom";
 import { HiOutlineX } from "react-icons/hi";
 
-const Modal = ({ open, onClose, children }) => {
+const Modal = ({ open, onClose, children, title }) => {
     return createPortal(
         <div
             onClick={onClose}
@@ -10,17 +10,20 @@ const Modal = ({ open, onClose, children }) => {
             }`}
         >
             <div
-                className={`bg-white rounded-xl shadow p-5 w-[calc(100vw-1rem)] max-w-screen-sm sm:min-w-[350px] sm:w-fit transition-all max-h-[calc(100vh-1rem)] overflow-y-auto ${
+                className={`bg-white rounded-xl shadow p-5 w-[calc(100vw-1rem)] max-w-screen-sm sm:min-w-[350px] sm:w-auto transition-all max-h-[calc(100vh-1rem)] overflow-y-auto ${
                     open ? "scale-100 opacity-100" : "scale-110 opacity-0"
                 }`}
                 onClick={(e) => e.stopPropagation()}
             >
-                <button
-                    className="absolute top-2 right-2 p-1 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600 text-xl duration-300"
-                    onClick={onClose}
-                >
-                    <HiOutlineX />
-                </button>
+                <div className="flex items-center justify-between border-b border-b-secondary-200 pb-2">
+                    <h3 className="text-lg truncate font-medium">{title}</h3>
+                    <button
+                        className="p-1 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600 text-2xl duration-300"
+                        onClick={onClose}
+                    >
+                        <HiOutlineX />
+                    </button>
+                </div>
                 <div className="mt-5">{children}</div>
             </div>
         </div>,
