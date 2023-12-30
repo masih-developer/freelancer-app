544aba93d7e04c779522413291cc107152c3ab57
masih abedini <masih7816@gmail.com>
2023-12-31 01:15:53 +0330

    create PageLoading component

---
+++ src/ui/PageLoading.jsx
@@ -0,0 +1,11 @@
+import Loading from "./Loading";
+
+const PageLoading = () => {
+  return (
+    <div className="flex min-h-[calc(100vh-56px)] w-full items-center justify-center">
+      <Loading size={18} />
+    </div>
+  );
+};
+
+export default PageLoading;
