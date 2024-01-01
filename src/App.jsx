4edace66c06f22f71e779b447d32e0f550146b69
masih abedini <masih7816@gmail.com>
2024-01-01 16:23:23 +0330

    change Owner component name to OwnerDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Auth from "./pages/Auth";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/CompleteProfile";
 import NotFound from "./pages/NotFound";
-import Owner from "./pages/Owner";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import OwnerLayout from "./layouts/OwnerLayout";
 import Home from "./pages/Home";
+import OwnerDashboard from "./pages/OwnerDashboard";
 
 const App = () => {
   return (
@@ -20,7 +20,7 @@ const App = () => {
         {/* owner layout Routes */}
         <Route path="/owner" element={<OwnerLayout />}>
           <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<Owner />} />
+          <Route path="dashboard" element={<OwnerDashboard />} />
           <Route path="projects">
             <Route index element={<Projects />} />
             <Route path=":projectId" element={<Project />} />
